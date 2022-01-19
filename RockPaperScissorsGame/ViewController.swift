198fd74ea59dd9bb219eec74379a24dd42a0fa08
Fola Flor <dpcalfola@gmail.com>
2022-01-19 20:42:06 +0900

    ver 0.15
    style >
    insert text on com & user label
    adjust constraints Reset&Select button
    
    feat >
    make variable LabelView
    connect buttons to func on ViewController

--- RockPaperScissorsGame/ViewController.swift
+++ RockPaperScissorsGame/ViewController.swift
@@ -14,18 +14,38 @@ class ViewController: UIViewController {
 	@IBOutlet weak var comImageView: UIImageView!
 	@IBOutlet weak var userImageView: UIImageView!
 
+	@IBOutlet weak var comLabelView: UILabel!
+	@IBOutlet weak var userLabelView: UILabel!
+
+
+
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
 
 		comImageView.image = #imageLiteral(resourceName: "readyLeft")
 		userImageView.image = #imageLiteral(resourceName: "readyRight")
+		comLabelView.text = "Ready"
+		userLabelView.text = "Ready"
+
+
 
 		// Do any additional setup after loading the view.
+
+	}//viewDidLoad
+
+
+	@IBAction func userChoiceButtonTapped(_ sender: UIButton) {
+		print("user")
 	}
 
+	@IBAction func resetButtonTapped(_ sender: UIButton) {
+		print("reset")
+	}
 
-
-
+	@IBAction func selectButtonTapped(_ sender: UIButton) {
+		print("select")
+	}
 
 
 }
